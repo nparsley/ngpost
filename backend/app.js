8a3743ae305d9f255075d8e9cab3c5862c22d6d8
nparsley <nickparsley5@gmail.com>
2021-07-11 16:50:30 -0700

    node,express setup + connect

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('first middleware')
+  next();
+});
+
+
+
+app.use((req, res, next) => {
+  res.send('hello express');
+});
+
+
+module.exports = app;
+
+
+
+
