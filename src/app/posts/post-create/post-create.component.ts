ce968a22f993f7dae88013ee930baee5584d23e7
nparsley <nickparsley5@gmail.com>
2021-07-10 19:00:50 -0700

    form edit

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Post } from "../post.model";
+import { PostService } from "../posts.service";
 
 @Component({
   selector: 'app-post-create',
@@ -10,18 +11,23 @@ import { Post } from "../post.model";
 export class PostCreateComponent {
   enteredContent = '';
   enteredTitle = '';
-  @Output() postCreated = new EventEmitter<Post>();
+  // @Output() postCreated = new EventEmitter<Post>();
+
+
+  constructor(public postService: PostService) {}
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
       return;
     }
 
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content
-    };
-    this.postCreated.emit(post);
+    // const post: Post = {
+    //   title: form.value.title,
+    //   content: form.value.content
+    // };
+    // this.postCreated.emit(post);
+    this.postService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,22 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Post } from '../post.model';
+import { PostService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   {title: 'first post', content: 'this is the first post\'s conent'},
   //   {title: 'second post', content: 'this is the second post\'s conent'},
   //   {title: 'third post', content: 'this is the third post\'s conent'},
   // ]
-  @Input() posts: Post[] = [];
+  // @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  // postsService: PostService;
+  private postSub: Subscription;
 
-  constructor() { }
+  constructor(public postsService: PostService) {
+    // this.postsService = postsService;
+  }
 
   ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+
+    this.postSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
   }
+
+  ngOnDestroy() {
+    this.postSub.unsubscribe();
+  }
+
 
 }
