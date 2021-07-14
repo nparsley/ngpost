6534df958b8e42c2cc69e6d023a0b8a7fc06e1e1
nparsley <nickparsley5@gmail.com>
2021-07-14 14:57:35 -0700

    post backend + angular add

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  next();
+});
 
 // app.use((req, res, next) => {
 //   console.log('first middleware')
 //   next();
 // });
 
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'post added successfully'
+  });
+});
 
 
-app.use('/api/posts', (req, res, next) => {
+
+// app.use('/api/posts', (req, res, next) => {
+  app.get('/api/posts', (req, res, next) => {
   // res.send('hello express');
 
   const posts = [
