a44cda015ab4ea77771db633f80d21047fd451f2
nparsley <nickparsley5@gmail.com>
2021-08-05 19:25:29 -0700

    procfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // apiUrl: "http://postapp-env.eba-fcjy24fi.us-east-2.elasticbeanstalk.com/api"
-  apiUrl: 'https://postappng.herokuapp.com/api'
+  // apiUrl: 'https://postappng.herokuapp.com/api'
 };
 
 
