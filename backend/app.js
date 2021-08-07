1695edb72432dfb12f2bbbc2f5596002579b81bc
nparsley <nickparsley5@gmail.com>
2021-08-07 12:22:43 -0700

    deploy-integrated

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,9 @@ mongoose.connect("mongodb+srv://postappng:" + process.env.MONGO_ATLAS_PW + "@clu
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/images', express.static(path.join('images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+// integrated angular
+app.use('/', express.static(path.join(__dirname, 'angular')));
 
 
 
@@ -40,6 +42,10 @@ app.use((req, res, next) => {
 
 app.use('/api/posts', postsRoutes);
 app.use('/api/user', userRoutes);
+// handle with angular
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'angular', 'index.html'));
+});
 
 
 module.exports = app;
